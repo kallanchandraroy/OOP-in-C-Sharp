d602e710d76024e8ca8642425368ae2bc08b93a5
Kallan <kallanchandraroy@gmail.com>
2021-05-15 13:19:06 +0600

    Add project files.

---
+++ OOP in C-Sharp/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_in_C_Sharp
+{
+    class Person
+    {
+        public string firstname;
+        public string middlename;
+        public string lastname;
+
+        public string GetFullName()
+        {
+            string FullName = firstname + " " + middlename + " " + lastname;
+            return FullName;
+        }
+    }
+}
---
+++ OOP in C-Sharp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_in_C_Sharp
+{
+    //OOP Example Class and Object
+
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+           
+
+        }
+    }
+}
